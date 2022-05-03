04c1eae3090db318e8f9b9428c6f7f677e059152
BhuviGoel <73011205+BhuviGoel@users.noreply.github.com>
2022-05-03 16:20:40 +0530

    for range

--- main.py
+++ main.py
@@ -17,17 +17,47 @@ def name():
         "data": star_data,
     }), 200
 
-# @app.route("/type")
-# def star_type():
-#     star_type = request.args.get("star_type")
-#     star_data = []
-#     for star in data:
-#         if star["star_type"] == star_type:
-#             star_data.append(star)
-#     return jsonify({
-#         "data": star_data,
-#         "message": "success"
-#     }), 200
+@app.route("/gravity")
+def gravity():
+    gravity = request.args.get("range")
+    lesser = gravity.split("-")[0]
+    greater = gravity.split("-")[1]
+    star_data = []
+    for star in data:
+        if star["gravity"] > lesser:
+            if star["gravity"] < greater:
+                star_data.append(star)
+    return jsonify({
+        "data": star_data,
+    }), 200
+
+@app.route("/mass")
+def mass():
+    mass = request.args.get("range")
+    lesser = mass.split("-")[0]
+    greater = mass.split("-")[1]
+    star_data = []
+    for star in data:
+        if star["mass"] > lesser:
+            if star["mass"] < greater:
+                star_data.append(star)
+    return jsonify({
+        "data": star_data,
+    }), 200
+
+@app.route("/radius")
+def radius():
+    radius = request.args.get("range")
+    lesser = radius.split("-")[0]
+    greater = radius.split("-")[1]
+    star_data = []
+    for star in data:
+        if star["radius"] > lesser:
+            if star["radius"] < greater:
+                star_data.append(star)
+    return jsonify({
+        "data": star_data,
+    }), 200
 
 if __name__ == "__main__":
     app.run()
